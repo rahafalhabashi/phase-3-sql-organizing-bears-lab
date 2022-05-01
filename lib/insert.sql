428184ccd58d640b2dda356d84e37617298ceb2a
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-05-01 14:34:00 -0400

    commit tree 88d16e561cf791e57a335824b20fc60197656869

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1,"Mr. Chocolate", 1, "F", "green", "cool", 1),
+(2,"Rowdy", 3, "F", "chill", "blue", 1),
+(3,"Tabitha", 1, "F", "angry", "black", 0),
+(4,"Sergeant Brown", 12, "M", "silver", "cool", 0),
+(5,"Melissa", 11, "M", "cream", "cool", 1),
+(6,"Grinch", 15, "M", "siyah", "happy", 1),
+(7,"Wendy", 41, "F", "maroon", "sad", 1),
+(8,null, 65, "F", "red", "pouty", 0);
+
+
